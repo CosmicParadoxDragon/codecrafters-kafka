bdc8a207f8054228d9fc1ad2d875f714f43618e0
Sohrab Kazak <sohrab.kazak@gmail.com>
2024-10-07 13:44:08 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,19 +1,25 @@
 import socket  # noqa: F401
 
-def create_message(correlation_id):
+def create_message(correlation_id, error_code=0):
     id_bytes = correlation_id.to_bytes(4, byteorder='big')
-    return len(id_bytes).to_bytes(4, byteorder='big') + id_bytes
+    error_code_bytes = error_code.to_bytes(2, byteorder='big')
+    return len(id_bytes + error_code_bytes).to_bytes(4, byteorder='big') + id_bytes + error_code_bytes
 
 def client_handler(client, addr):
     data = client.recv(1024)
     message_length = int.from_bytes(data[:4], byteorder='big')
     request_api_key = int.from_bytes(data[4:6], byteorder='big')
-    request_api_version = int.from_bytes(data[6:8], byteorder='big')
+    request_api_version = version_check(int.from_bytes(data[6:8], byteorder='big'))
     correlation_id = int.from_bytes(data[8:12], byteorder='big')
+    if not request_api_version:
+        client.sendall(create_message(correlation_id=correlation_id, error_code=35))
     # client_id
     # tagged_fields
     print(data)
     client.sendall(create_message(correlation_id))
+
+def version_check(request_api_version):
+    return request_api_version if request_api_version in [0, 1, 2, 3, 4] else False
 
 def main():
     # You can use print statements as follows for debugging,
